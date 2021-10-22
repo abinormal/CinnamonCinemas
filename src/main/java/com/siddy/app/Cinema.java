7478464cff493387d5e6fa20d1011a3056228754
Abi Sidford <asidford@gmail.com>
2021-10-22 11:35:07 +0100

    Add function to add seats to the array

--- src/main/java/com/siddy/app/Cinema.java
+++ src/main/java/com/siddy/app/Cinema.java
@@ -1,12 +1,47 @@
 package com.siddy.app;
 
+import java.util.Arrays;
+
 public class Cinema {
 
-    public int allocateSeats(int seats){
+    private int[][] seatingArray = new int[3][5];
+    private final int seatsLeft = 15;
+    private final int maxSeatsAlloc = 3;
+
+    public Cinema() {
+        for (int[] row : seatingArray) {
+            Arrays.fill(row, 0);
+        }
+    }
+
+    public int allocateSeats(int seats) {
         // Check seats allowed are only 1-3
-        if (seats < 1 || seats >3){
+        if (seats < 1 || seats > maxSeatsAlloc) {
             return 0;
         }
-        return 1;
+        return addToArray(seats);
+    }
+
+    private int addToArray(int seatsRequested) {
+        int seatsAllocated = 0;
+        if (seatsLeft >= seatsRequested) {
+            // Break label
+            breakOutOfNestedLoop:
+            for (int[] row : seatingArray) {
+                for (int seat : row) {
+                    if (seat == 0) {
+                        seat = 45;
+                        seatsAllocated++;
+                        seatsRequested--;
+                        System.out.print("Seat:" + seat + " ");
+                    }
+                    if (seatsRequested <= 0) {
+                        break breakOutOfNestedLoop;
+                    }
+                }
+            }
+        }
+        return seatsAllocated;
     }
 }
+
